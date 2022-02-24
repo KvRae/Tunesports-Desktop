9ea680192e8347b98f0ed9e7ef6e9ed59aea414d
Restyled.io <commits@restyled.io>
2022-02-24 20:12:43 +0000

    Restyled by clang-format

--- src/entities/Evenement.java
+++ src/entities/Evenement.java
@@ -6,89 +6,62 @@
 package entities;
 import java.util.Date;
 public class Evenement {
-    private int id_event ;
-    private String nomevent;
-    private Date datedebevent;
-    private Date datefinevent;
-    private String descevent;
-    //private String utilisateur;
-    
-    
-    
-    
+  private int id_event;
+  private String nomevent;
+  private Date datedebevent;
+  private Date datefinevent;
+  private String descevent;
+  // private String utilisateur;
 
-    public Evenement() {
-    }
+  public Evenement() {}
 
-    public Evenement(int id_event, String nomevent,Date datedebevent,Date datefinevent, String descevent) {
-        this.id_event = id_event;
-        this.nomevent=nomevent;
-        this.datedebevent = datedebevent;
-        this.datefinevent = datefinevent;
-        this.descevent = descevent;
-    }
+  public Evenement(int id_event, String nomevent, Date datedebevent,
+                   Date datefinevent, String descevent) {
+    this.id_event = id_event;
+    this.nomevent = nomevent;
+    this.datedebevent = datedebevent;
+    this.datefinevent = datefinevent;
+    this.descevent = descevent;
+  }
 
-    public Evenement(int id_event) {
-        this.id_event = id_event;
-    }
+  public Evenement(int id_event) { this.id_event = id_event; }
 
-    public Evenement(int id_event, String nomevent, String descevent) {
-        this.id_event = id_event;
-        this.nomevent = nomevent;
-        this.descevent = descevent;
-    }
+  public Evenement(int id_event, String nomevent, String descevent) {
+    this.id_event = id_event;
+    this.nomevent = nomevent;
+    this.descevent = descevent;
+  }
 
-    public Evenement(Date datedebevent) {
-        this.datedebevent = datedebevent;
-    }
-    
+  public Evenement(Date datedebevent) { this.datedebevent = datedebevent; }
 
+  public int getId_event() { return id_event; }
 
-    public int getId_event() {
-        return id_event;
-    }
+  public String getNomevent() { return nomevent; }
 
-    public String getNomevent() {
-        return nomevent;
-    }
+  public Date getDatedebevent() { return datedebevent; }
 
-    public Date getDatedebevent() {
-        return datedebevent;
-    }
+  public Date getDatefinevent() { return datefinevent; }
 
-    public Date getDatefinevent() {
-        return datefinevent;
-    }
-    
-    public String getDescevent() {
-        return descevent;
-    }
+  public String getDescevent() { return descevent; }
 
-    public void setId_event(int id_event) {
-        this.id_event = id_event;
-    }
+  public void setId_event(int id_event) { this.id_event = id_event; }
 
-    public void setNomevent(String nomevent) {
-        this.nomevent = nomevent;
-    }
+  public void setNomevent(String nomevent) { this.nomevent = nomevent; }
 
-    public void setDatedebevent(Date datedebevent) {
-        this.datedebevent = datedebevent;
-    }
+  public void setDatedebevent(Date datedebevent) {
+    this.datedebevent = datedebevent;
+  }
 
-    public void setDatefinevent(Date datefinevent) {
-        this.datefinevent = datefinevent;
-    }
+  public void setDatefinevent(Date datefinevent) {
+    this.datefinevent = datefinevent;
+  }
 
-    public void setDescevent(String descevent) {
-        this.descevent = descevent;
-    }
-    @Override
-    public String toString() {
-        return "Evenement{" + "id_event=" + id_event + ", nomevent=" + nomevent + ", datedebevent=" + datedebevent + ", datefinevent=" + datefinevent + ", descevent=" + descevent + '}';
-    }
-    
-    
-   
-    
+  public void setDescevent(String descevent) { this.descevent = descevent; }
+  @Override
+  public String toString() {
+    return "Evenement{"
+        + "id_event=" + id_event + ", nomevent=" + nomevent +
+        ", datedebevent=" + datedebevent + ", datefinevent=" + datefinevent +
+        ", descevent=" + descevent + '}';
+  }
 }
--- src/tests/Main.java
+++ src/tests/Main.java
@@ -5,27 +5,26 @@
  */
 package tests;
 
-import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.web.WebView;
-import javafx.scene.layout.VBox; 
+import static java.lang.String.valueOf;
 
-import java.sql.SQLException;
 import Services.EvenementService;
 import Services.TournoiService;
 import com.itextpdf.text.DocumentException;
 import entities.Evenement;
 import entities.Tournoi;
 import java.io.FileNotFoundException;
-import static java.lang.String.valueOf;
+// import java.util.Date;
+import java.sql.Date;
+import java.sql.SQLException;
 import java.text.DateFormat;
 import java.text.ParseException;
-//import java.util.Date;
-import java.sql.Date;
-import tools.MaConnexion;
 import java.text.SimpleDateFormat;
-import java.text.ParseException;
+import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.scene.web.WebView;
+import javafx.stage.Stage;
+import tools.MaConnexion;
 
 /**
  *
@@ -33,74 +32,70 @@ import javafx.scene.Scene;
  */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     * @throws java.text.ParseException
-     */
-    public static void main(String[] args) throws ParseException, FileNotFoundException, DocumentException {
-        MaConnexion mc = MaConnexion.getInstance();
-          //Personne p = new Personne("Ben Ahmed", "Ali");
-          EvenementService es = new EvenementService();
-          TournoiService ts=new TournoiService();
-          
+  /**
+   * @param args the command line arguments
+   * @throws java.text.ParseException
+   */
+  public static void main(String[] args)
+      throws ParseException, FileNotFoundException, DocumentException {
+    MaConnexion mc = MaConnexion.getInstance();
+    // Personne p = new Personne("Ben Ahmed", "Ali");
+    EvenementService es = new EvenementService();
+    TournoiService ts = new TournoiService();
 
-          
-          java.sql.Date dde=java.sql.Date.valueOf("2021-12-12");
-          java.sql.Date dfe=java.sql.Date.valueOf("2022-02-25");
-          
-          java.sql.Date ddt=java.sql.Date.valueOf("2020-01-12");
-          java.sql.Date dft=java.sql.Date.valueOf("2019-05-05");
-          
-          
-          Evenement e= new Evenement(1,"even9",dde,dfe,"2éme");
-          Tournoi t=new Tournoi(1,"tournoi_9",ddt,dft,"hajhdkdal","gfgdqsf");
-          Tournoi t1=new Tournoi(5);
-          Tournoi t2=new Tournoi(1,"tournoi_1",ddt,dft,"kkkkkkkk","ddddddddd");
-          /*Evenement e1= new Evenement(4,"even2",datedebevent,datefinevent,"2éme");
-          Evenement e2= new Evenement(18,"XXXXX",datedebevent,datefinevent,"XXXXX");
-          Evenement e3= new Evenement(0,"even3",datedebevent,datefinevent,"3éme");
-          Tournoi t1=new Tournoi(2,"tournoi_2",datedebtour,datefintour,"dfsqgvwvw","ererrerere");
-          Tournoi t2=new Tournoi(3,"tournoi_2",datedebtour,datefintour,"jjkjjkjkjkj","dsddsdsdsd");
-          Tournoi t3=new Tournoi(3,"XXXXXXXX",datedebtour,datefintour,"XXXXXXXX","XXXXXXXX");*/
-          
-        // es.pdf(e);
-        ts.pdf(t);
-         // es.ajouter(e);
-          //es.ajouter(e1);
-          //es.ajouter(e2);
-         // es.supprimer(e3);
-          //es.modifier(e2);
-        // es.recherche(e);
-        
-        
-       //es.historique();
-       // System.out.println(es.historique());
-        
-        
-        // ts.ajouter(t);
-          //ts.ajouter(t1);
-          //ts.ajouter(t2);
-         //ts.ajouter(t2);
-          //ts.supprimer(t1);
-         // ts.modifier(t2);
-        // ts.recherche(t1);
-        //ts.historique();
-       // System.out.println(ts.historique());
-           // es.triedesc();
-          //System.out.println(es.triedesc());
-          //es.trie();
-           //System.out.println(es.trie());
-           
-           //es.triedesc();
-           //System.out.println(ts.triedesc());
-           
-          //ts.trie();
-           //System.out.println(ts.trie());
-           
-          //es.afficher();
-          //System.out.println(es.afficher());
-        
-    }
-   
-    
+    java.sql.Date dde = java.sql.Date.valueOf("2021-12-12");
+    java.sql.Date dfe = java.sql.Date.valueOf("2022-02-25");
+
+    java.sql.Date ddt = java.sql.Date.valueOf("2020-01-12");
+    java.sql.Date dft = java.sql.Date.valueOf("2019-05-05");
+
+    Evenement e = new Evenement(1, "even9", dde, dfe, "2éme");
+    Tournoi t = new Tournoi(1, "tournoi_9", ddt, dft, "hajhdkdal", "gfgdqsf");
+    Tournoi t1 = new Tournoi(5);
+    Tournoi t2 = new Tournoi(1, "tournoi_1", ddt, dft, "kkkkkkkk", "ddddddddd");
+    /*Evenement e1= new Evenement(4,"even2",datedebevent,datefinevent,"2éme");
+    Evenement e2= new Evenement(18,"XXXXX",datedebevent,datefinevent,"XXXXX");
+    Evenement e3= new Evenement(0,"even3",datedebevent,datefinevent,"3éme");
+    Tournoi t1=new
+    Tournoi(2,"tournoi_2",datedebtour,datefintour,"dfsqgvwvw","ererrerere");
+    Tournoi t2=new
+    Tournoi(3,"tournoi_2",datedebtour,datefintour,"jjkjjkjkjkj","dsddsdsdsd");
+    Tournoi t3=new
+    Tournoi(3,"XXXXXXXX",datedebtour,datefintour,"XXXXXXXX","XXXXXXXX");*/
+
+    // es.pdf(e);
+    ts.pdf(t);
+    // es.ajouter(e);
+    // es.ajouter(e1);
+    // es.ajouter(e2);
+    // es.supprimer(e3);
+    // es.modifier(e2);
+    // es.recherche(e);
+
+    // es.historique();
+    //  System.out.println(es.historique());
+
+    // ts.ajouter(t);
+    // ts.ajouter(t1);
+    // ts.ajouter(t2);
+    // ts.ajouter(t2);
+    // ts.supprimer(t1);
+    // ts.modifier(t2);
+    // ts.recherche(t1);
+    // ts.historique();
+    // System.out.println(ts.historique());
+    // es.triedesc();
+    // System.out.println(es.triedesc());
+    // es.trie();
+    // System.out.println(es.trie());
+
+    // es.triedesc();
+    // System.out.println(ts.triedesc());
+
+    // ts.trie();
+    // System.out.println(ts.trie());
+
+    // es.afficher();
+    // System.out.println(es.afficher());
+  }
 }
