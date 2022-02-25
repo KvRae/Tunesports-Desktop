0754dc5a49135ffa6b633fc6a8a2a6ff5af6b54b
firas1239 <63450905+firas1239@users.noreply.github.com>
2022-02-25 12:11:36 +0100

    Squashed commit of the following:
    commit f65467c550afef186f612a726683f3ea91430862
    Author: firas1239 <63450905+firas1239@users.noreply.github.com>
    Date:   Fri Feb 25 12:10:03 2022 +0100
    
        cruud+metiers
    
    commit 3b66307313c11add0a6ac8ec2a4e24176c3d53be
    Author: firas1239 <63450905+firas1239@users.noreply.github.com>
    Date:   Fri Feb 25 12:03:27 2022 +0100
    
        Delete src directory

--- src/Tests/Main.java
+++ src/Tests/Main.java
@@ -1,48 +1,100 @@
 /*
- *  Copyright (c) 2022.
- *  Written By KvRae.
- * I hate writing documentations.
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
-
 package Tests;
-import Entities.Reservation;
-import Services.ReservationService;
+
+import java.sql.SQLException;
+
+import com.itextpdf.text.DocumentException;
+
+import Entities.Jeux;
+import Entities.Livraison;
+import Entities.Mise_a_jour;
+import Services.LivraisonService;
+import Services.JeuxService;
+import Services.MiseAJourService;
 import Tools.MaConnexion;
 
+import java.sql.Date;
+import static java.lang.String.valueOf;
+
+import java.io.FileNotFoundException;
+
+
+/**
+ *
+ * @author 
+ */
 public class Main {
-    public static void main(String[] args) {
-        //insuring singleton connection to our Db Server
-        MaConnexion cnx = MaConnexion.getInstance();
-        //***************************Creating Test Instance for reservation************************************
-        Reservation re = new Reservation(new java.util.Date() ,"Disponible",10);
-        //Reservation re2 = new Reservation(2,new java.util.Date() , Disponiblity.valueOf("Disponible"),15);
-        //Reservation re1 = new Reservation(3,new java.util.Date(),Disponiblity.valueOf("Disponible"),10);
-        Reservation re3 = new Reservation(6);
-        ReservationService res = new ReservationService();
 
-        //********************Testing Service Reservation*************************
+    /**
+     * @param args the command line arguments
+     * @throws SQLException 
+     * @throws DocumentException 
+     * @throws FileNotFoundException 
+     */
+    public static void main(String[] args) throws SQLException, FileNotFoundException, DocumentException {
+        MaConnexion mc = MaConnexion.getInstance();
+        
+        java.sql.Date dde=java.sql.Date.valueOf("2021-12-12");
+        java.sql.Date ch=java.sql.Date.valueOf("2000-05-15");
 
-        System.out.println("-----------------------------------------------");
-        //res.ajouter(re);
-        res.recherche(re3);
-        //res.modifier(re2);
-        //res.supprimer(re3);
-        //System.out.println(res.afficher());
 
-        //***************************Creating Test Instance for rating************************************
+        
+        Jeux J1 =new Jeux ("free fire",ch,17,"tttttyyy","Pc and Mobile ps5","chiheb");
+        Jeux J2 =new Jeux ("PUBG",dde,17,"test","Pc and Mobile and ps4","firas1");
+        Jeux J3 =new Jeux (1,"PUBG",dde,155,"test", "OS5","firas1");
+        Jeux J4=new Jeux(1);
+        Jeux J6 =new Jeux ("fortnite",dde,40,"test", "Mobile and ps4 and pc","game good");
+        
+        
+        
+        
 
-        //Rating ra = new Rating("12/8/2022", "12:00",4);
-        //Rating ra2 = new Rating(2,"User5", new java.util.Date(),1);
-        //Rating ra1 = new Rating(1);
-        //RatingService ras = new RatingService();
+Mise_a_jour m3=new Mise_a_jour(8);
 
-        //********************Testing Service Rating*************************
 
-        //System.out.println("-----------------------------------------------");
-        //ras.ajouter(ra2);
-        //ras.modifier(ra2);
-        //ras.supprimer(ra1);
-        //System.out.println(ras.afficher());
 
+
+     Livraison L2 =new Livraison (1,"achour",941,3,655,"Ariana");
+
+
+
+
+
+
+          JeuxService es = new JeuxService();
+          MiseAJourService em =new MiseAJourService();
+          LivraisonService ls =new LivraisonService();
+          //em.ajouter(m3);
+          //es.ajouter(J1);
+          //es.supprimer(J4);
+         // em.supprimer(m3);
+          //ls.ajouter(L2);
+          
+
+         //es.modifier(J3);
+          //em.modifier(m2);
+          //System.out.println(es.afficher());
+          //System.out.println(em.afficher());
+         // System.out.println(ls.afficher());
+          //ls.pdf(L2);
+          //es.recherche(J4);
+          //em.recherche(m3);
+          System.out.println(em.triedesc());
+          
+
+          
+          //em.affichejeuxmiseajour(8);
+        // System.out.println(em.affichejeuxmiseajour(8));
+
+        
+          
+          
+
+				
     }
+    
 }
