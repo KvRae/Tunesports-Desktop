48708cf38bacc809e34a35811bbdab0221abc52c
KvRae <58667227+KvRae@users.noreply.github.com>
2022-02-25 12:34:43 +0100

    Squashed commit of the following:
    commit 1fcede6f8ff150c2f2a992b2db58536bc7491c71
    Author: KvRae <58667227+KvRae@users.noreply.github.com>
    Date:   Fri Feb 25 12:33:20 2022 +0100
    
        updating

---
+++ src/Services/RatingService.java
@@ -0,0 +1,101 @@
+/*
+ *  Copyright (c) 2022.
+ *  Written By KvRae.
+ * I hate writing documentations.
+ */
+
+package Services;
+
+import Entities.Rating;
+import Tools.MaConnexion;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RatingService implements IService<Rating> {
+//******************************Start Connection Instance*****************************
+    Connection cnx= MaConnexion.getInstance().getCnx();
+
+//*******************************************************************************************
+//*******************************Crud Operations**************************************
+//*******************************************************************************************
+
+//********************************Create Operation*****************************************
+    @Override
+    public void ajouter(Rating ra) {
+        PreparedStatement pre;
+        try {
+            pre = cnx.prepareStatement("INSERT INTO rating (titleRat,dateRat,valueRat)VALUES (?,?,?);");
+            pre.setString(1, ra.getTitleRat());
+            pre.setDate(2,  new Date(ra.getDateRat().getTime()));
+            pre.setInt(3, ra.getValueRat());
+            System.out.println("Rating Added");
+            pre.executeUpdate();
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+
+        }
+    }
+
+    //***************************Read Operation*****************************************
+    @Override
+    public List<Rating> afficher() {
+        List<Rating> ratings = new ArrayList<>();
+        String sql ="select * from rating";
+        try {
+            Statement ste= cnx.createStatement();
+            ResultSet rs =ste.executeQuery(sql);
+            while(rs.next()){
+                Rating ra = new Rating();
+                ra.setTitleRat(rs.getString("titleRat"));
+                ra.setDateRat(rs.getDate("dateRat"));
+                ra.setValueRat(rs.getInt("valueRat"));
+                ratings.add(ra);
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return ratings;}
+
+    //***************************Update Operation*****************************************
+    @Override
+    public void modifier(Rating ra) {
+        String query = "UPDATE rating SET titleRat = '" + ra.getTitleRat() + "', dateRat = '" +
+                ra.getDateRat() + "', valueRat= '" + ra.getValueRat() +
+                "' WHERE idRat = " + ra.getIdRat() + "";
+        try{
+            Statement ste = cnx.createStatement();
+            ste.executeUpdate(query);
+            System.out.println("Rating modifieé ");
+        }
+        catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    //***************************Delete Operation*****************************************
+    @Override
+    public void supprimer(Rating ra) {
+        String query = "DELETE FROM rating WHERE idRat = " + ra.getIdRat();
+        try{
+            Statement ste = cnx.createStatement();
+            ste.executeUpdate(query);
+            System.out.println("Rating supprimer");
+        }
+        catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    //***************************Search Operation*****************************************
+    @Override
+    public void recherche(Rating rating) {
+
+    }
+
+}
