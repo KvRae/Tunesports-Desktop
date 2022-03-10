7171f629d4eca1b51cbbc2f90203b5ec2fcbdffb
youssef <youssef.achour1@esprit.tn>
2022-03-10 23:57:00 +0100

    last commit

---
+++ src/Gui/mailling.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Gui;
+
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+/**
+ *
+ * @author achou
+ */
+public class mailling {
+    public static void sendMail(String recepient) throws Exception {
+        System.out.println("Preparing to send email");
+        Properties properties = new Properties();
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.smtp.host", "smtp.gmail.com");
+        properties.put("mail.smtp.port", "587");
+        String myAccountEmail = "youssef.achour1@esprit.tn";
+        String password = "191JMT2807";
+
+        Session session = Session.getInstance(properties, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(myAccountEmail, password);
+            }
+        });
+        Message message = prepareMessage(session, myAccountEmail, recepient);
+        Transport.send(message);
+        System.out.println("Message sent successfully");
+    }
+
+    private static Message prepareMessage(Session session, String myAccountEmail, String recepient) {
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(myAccountEmail));
+            message.setRecipient(Message.RecipientType.TO, new InternetAddress(recepient));
+            message.setSubject("INFORMATION");
+            CommandeController vl =new CommandeController();
+            message.setText("votre commande est  "+ vl.validation()+"" );
+            //message.setText(pc.getSum());
+            return message;
+        } catch (Exception ex) {
+            Logger.getLogger(mailling.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+        
+    }
+    
+    
+}
